b1ce672ba1fba20fadccffd666c12489d42f1611
B3rt-k <edbert@infini88.net>
2025-08-22 12:17:14 -0500

    files modified

--- Assignment1.py
+++ Assignment1.py
@@ -9,25 +9,33 @@ Creation Date: 8/22/2025
 '''
 
 class MaxHeap:
-    def __init__(self,i):
+    def __init__(self):
         self.arr = []       # initialize an empty list when MaxHeap object is created
-        self.i = i          # index i variable is tracked to act as pointer
+    
+    # i should just be a variable that is the position or index the newly inserted node.
+    def heapify(self, i):    
+        # the definition of the parent property of Binary Heaps is (index - 1) floor division by 2
+        parent = (i - 1) // 2   
 
-    def heapify(self):
-        parent = (self.i - 1) // 2
-
-        if parent >= 0 and self.arr[i] > self.arr[parent]:
-            self.arr[self.i], self.arr[parent] = self.arr[parent], self.arr[self.i]
+        # A condition that checks if the parent is positie and whatever index i is bigger than the parent value
+        if parent >= 0 and self.arr[i] > self.arr[parent]:       
+            self.arr[i], self.arr[parent] = self.arr[parent], self.arr[i]
         
         # Recursively heapify the parent node
-            MaxHeap.heapify(self.arr, parent)
+            self.heapify(parent)
 
     def insertNode(self,key):
         self.arr.append(key)
-
-        MaxHeap.heapify(self.arr, len(self.arr)- 1 )
+        
+        #recursive
+        self.heapify( len(self.arr) - 1 )   
 
     def peek(self):
-        return self.arr[0]
+        return self.arr[0] if self.arr else None
     
-    def 
+if __name__ == '__main__':
+    arr = [ 3,10, 11, 40, 5, 8]
+    heap = MaxHeap()
+    for i in arr:
+        heap.insertNode(i)
+    print(heap.arr)
