50b7101eb141d311a83743dc2aad9da86d61c977
B3rt577 <edbert106@gmail.com>
2025-08-23 16:18:55 -0500

    added Priority Queue Class

--- Assignment1.py
+++ Assignment1.py
@@ -2,14 +2,14 @@
 Title: EECS 348 Assignment 1
 Description: CEO Email prioritization program
 Input(s): Assignment1_Test_Files.txt
-Output: Next email and number of unread emails
+Output(s): Next email and number of unread emails
 All collaborators: Geeks for geeks, ChatGPT
 Author's name: Edbert Jensen
 Creation Date: 8/22/2025
 '''
 
 '''
-Sources: https://www.geeksforgeeks.org/dsa/introduction-to-max-heap-data-structure/
+Sources for the MaxHeap: https://www.geeksforgeeks.org/dsa/introduction-to-max-heap-data-structure/
 
 '''
 class MaxHeap:
@@ -28,18 +28,26 @@ class MaxHeap:
         
         # Recursively heapify the parent node
             self.heapify_up(parent)
-
+    
+    # i should be the current index 
     def heapify_down(self, i):
         largest = i
         left = 2 * i + 1
         right = 2 * i + 2
         
+        # left 
         if ( left < len(self.arr) ) and ( self.arr[left] > self.arr[largest] ):
             largest = left
         
+        #
         if ( right < len(self.arr) ) and ( self.arr[left] > self.arr[largest] ):
+            largest = right
+        
+        if (largest != i):
+            self.arr[i],self.arr[largest]=self.arr[largest],self.arr[i]
 
-        pass
+            #Recursively heapify the affected sub-tree
+            self.heapify_down( largest )
 
     def insertNode(self,key):
         # Insert element to the list
@@ -49,15 +57,36 @@ class MaxHeap:
         self.heapify_up( len(self.arr) - 1 )   
 
     def deleteRoot(self):
+        arr_len = len(self.arr)
 
-        pass
+        lastElement = self.arr[arr_len-1]
+
+        self.arr[0] = lastElement
+
+        arr_len = arr_len - 1   
+
+        self.heapify_down(0)
 
     def peek(self):
         return self.arr[0] if self.arr else None
     
-if __name__ == '__main__':
-    arr = [ 3,10, 11, 40, 5, 8]
-    heap = MaxHeap()
-    for i in arr:
-        heap.insertNode(i)
-    print(heap.arr)
+
+class PriorityQueue:
+    def __init__(self):
+        self.heap = MaxHeap()
+
+    def enqueue(self, key):
+        # insert a new element to the Queue
+        self.heap.insertNode(key)
+    
+    def dequeue(self):
+        # return the highest priority element which is the root of a Binary Max Heap and heapify down the Binary Max Heap
+        self.heap.peek()
+        self.heap.heapify_down(0)
+    
+# if __name__ == '__main__':
+#     arr = [ 3,10, 11, 40, 5, 8]
+#     heap = MaxHeap()
+#     for i in arr:
+#         heap.insertNode(i)
+#     print(heap.arr)
